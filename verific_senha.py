ccc7323fb3bd1dfbec1de1ce64652633c8a318c7
Mandella-thi <73672494+Mandella-thi@users.noreply.github.com>
2023-02-02 10:55:33 -0300

    "verificador de senhas simples stack overflow e o meu modificado"

--- verific_senha.py
+++ verific_senha.py
@@ -11,5 +11,5 @@ while True:
     elif senha.isalnum():
         print("necessita de um Caractere especial: ")
     else:
-        print("senha aceita com sucesso 1!")
+        print("senha aceita com sucesso!!")
         break
